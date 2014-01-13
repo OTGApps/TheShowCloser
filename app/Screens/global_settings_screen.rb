bf6f9ea56f45d6f9eb5bf7c7dae7ffd47306e892
Mark Rickert <mjar81@gmail.com>
2014-01-13 17:15:13 -0500

    Add more items to the global settings page.

--- app/Screens/global_settings_screen.rb
+++ app/Screens/global_settings_screen.rb
@@ -47,7 +47,53 @@ class GlobalSettingsScreen < Formotion::FormController
           type: :text,
           value: App::Persistence['kReceiptName']
         }]
-      },{
+      }, {
+        title: "#{App.name} is open source:",
+        rows: [{
+          title: "View on GitHub",
+          type: :github_link,
+          image: "icon_github",
+          warn: true,
+          value: "https://github.com/MohawkApps/BeerJudge"
+        }, {
+          title: "Found a bug?",
+          subtitle: "Log it here.",
+          type: :issue_link,
+          image: "icon_issue",
+          warn: true,
+          value: "https://github.com/MohawkApps/BeerJudge/issues/"
+        }, {
+          title: "Email me suggestions!",
+          subtitle: "I'd love to hear from you",
+          type: :email_me,
+          image: "icon_email",
+          value: {
+            to: "mark@mohawkapps.com",
+            subject: "#{App.name} App Feedback"
+          }
+        }]
+      }, {
+        title: "Tell Your friends:",
+        rows: [{
+          title: "Share the app",
+          subtitle: "Text, Email, Tweet, or Facebook!",
+          type: :share,
+          image: "icon_share",
+          value: {
+            items: "I'm using the #{App.name} app. Check it out! http://www.mohawkapps.com/app/theshowcloser/",
+            excluded: [
+              UIActivityTypeAddToReadingList,
+              UIActivityTypeAirDrop,
+              UIActivityTypeCopyToPasteboard,
+              UIActivityTypePrint
+            ]
+          }
+        },{
+          title: "Rate on iTunes",
+          type: :rate_itunes,
+          image: "icon_itunes"
+        }]
+      }, {
         title: "About:",
         rows: [{
           title: "Version",
@@ -60,15 +106,6 @@ class GlobalSettingsScreen < Formotion::FormController
           font: { name: 'HelveticaNeue', size: 13 },
           placeholder: "#{copyright_year} Mohawk Apps, LLC",
           selection_style: :none
-        }, {
-          title: "Email me suggestions!",
-          subtitle: "I'd love to hear from you",
-          type: :email_me,
-          image: "email",
-          value: {
-            to: "mark@mohawkapps.com",
-            subject: "#{App.name} App Feedback"
-          }
         }, {
           title: "Visit MohawkApps.com",
           type: :web_link,
