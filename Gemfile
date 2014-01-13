2da9ecb2003e526e842c05afd08a5f67c7067975
Mark Rickert <mjar81@gmail.com>
2014-01-13 16:36:07 -0500

    Added lots to the global settings screen.

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'cocoapods'
 gem 'bubble-wrap', :github => 'rubymotion/BubbleWrap', :require => ['bubble-wrap/core', 'bubble-wrap/http', 'bubble-wrap/mail']
 gem 'ProMotion'#, github: 'clearsightstudio/ProMotion', branch: 'edge'
 gem 'motion_data_wrapper'
-gem 'formotion'
+gem 'formotion', :github => 'markrickert/formotion', :branch => 'texties'
 
 # Testflight
 # gem 'motion-testflight', :github => 'hboon/motion-testflight', :branch => 'takeoff-on-launch-only'
--- app/Screens/global_settings_screen.rb
+++ app/Screens/global_settings_screen.rb
@@ -1,5 +1,4 @@
 class GlobalSettingsScreen < Formotion::FormController
-  include BW::KVO
 
   def viewDidLoad
     super
@@ -8,7 +7,16 @@ class GlobalSettingsScreen < Formotion::FormController
     # Flurry.logEvent "SettingsView" unless Device.simulator?
   end
 
+  def set_defaults
+    App::Persistence['kTaxEnabled'] ||= true
+    App::Persistence['kTaxRate']    ||= 6.75
+    App::Persistence['kTaxShipping']||= true
+    App::Persistence['kReceiptName']||= "Your Favorite Jewelry Lady"
+  end
+
   def init
+    set_defaults
+
     @form ||= Formotion::Form.new({
       sections: [{
         title: "These settings will be the default for new hostesses. Changing these values will not affect previously created hostesses.",
@@ -19,21 +27,86 @@ class GlobalSettingsScreen < Formotion::FormController
           title: "Enable Tax?",
           key: :kTaxEnabled,
           type: :switch,
-          value: App::Persistence['kTaxEnabled'] || true
+          value: App::Persistence['kTaxEnabled']
+        },{
+          title: "Tax Rate(%)",
+          key: :kTaxRate,
+          type: :number,
+          value: App::Persistence['kTaxRate']
+        },{
+          title: "Tax Shipping?",
+          key: :kTaxShipping,
+          type: :switch,
+          value: App::Persistence['kTaxShipping']
+        }]
+      },{
+        title: "Preferences:",
+        rows: [{
+          title: "Your Name",
+          key: :kReceiptName,
+          type: :text,
+          value: App::Persistence['kReceiptName']
+        }]
+      },{
+        title: "About:",
+        rows: [{
+          title: "Version",
+          type: :static,
+          placeholder: App.info_plist['CFBundleShortVersionString'],
+          selection_style: :none
+        }, {
+          title: "Copyright",
+          type: :static,
+          font: { name: 'HelveticaNeue', size: 13 },
+          placeholder: "#{copyright_year} Mohawk Apps, LLC",
+          selection_style: :none
+        }, {
+          title: "Email me suggestions!",
+          subtitle: "I'd love to hear from you",
+          type: :email_me,
+          image: "email",
+          value: {
+            to: "mark@mohawkapps.com",
+            subject: "#{App.name} App Feedback"
+          }
+        }, {
+          title: "Visit MohawkApps.com",
+          type: :web_link,
+          warn: false,
+          value: "http://www.mohawkapps.com"
+        }, {
+          title: "Made with ♥ in North Carolina",
+          type: :static,
+          enabled: false,
+          selection_style: :none,
+          text_alignment: NSTextAlignmentCenter
+        },{
+          type: :static_image,
+          value: "nc",
+          enabled: false,
+          selection_style: :none
         }]
       }]
     })
     super.initWithForm(@form)
   end
 
-  def observe
-    tax_enabled = self.form.sections[1].rows[0]
-    observe(tax_enabled, "value") do |old_value, new_value|
-      App::Persistence['kTaxEnabled'] = new_value
-    end
+  def copyright_year
+    start_year = '2012'
+    this_year = Time.now.year
+
+    start_year == this_year ? this_year : "#{start_year}-#{this_year}"
   end
 
+
   def close
+    data = @form.render
+    ap data
+
+    data.each do |key, value|
+      App::Persistence[key.to_s] = value
+    end
+
     dismissModalViewControllerAnimated(true)
   end
 
