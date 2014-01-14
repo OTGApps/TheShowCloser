0c844e7609d82ac8929b7dc7487f2c620325aab6
Mark Rickert <mjar81@gmail.com>
2014-01-13 23:09:06 -0500

    Implemented IAP.

--- app/Helpers/jewelry_downloader.rb
+++ app/Helpers/jewelry_downloader.rb
@@ -23,6 +23,7 @@ class JewelryDownloader
     if version['free_update']
       free_upgrade
     else
+      @version = version
       paid_upgrade
     end
 
@@ -30,10 +31,39 @@ class JewelryDownloader
 
   def paid_upgrade
     ap 'starting paid upgrade'
+    BW::UIAlertView.new({
+      title: 'Catalog Update Available',
+      message: 'There is a PAID catalog update available.',
+      buttons: ['Cancel', 'Purchase'],
+      cancel_button_index: 0
+    }) do |alert|
+      if alert.clicked_button.cancel?
+        ap 'Canceled'
+      else
+        purchase_upgrade
+      end
+    end.show
+  end
+
+  def purchase_upgrade
+    Motion::Blitz.show('Purchasing Catalog Update')
+
+    @product = Vendor::Product.new(:id => @version['major'])
+    @product.purchase do |product|
+      p "Purchase successful: #{product.success}"
+      p "Purchase transaction: #{product.transaction}"
+
+      if product.success
+        Motion::Blitz.show('Thank you for your purchase. Downloading catalog update.')
+        download_and_save
+      else
+        Motion::Blitz.error('Transaction Cancelled.')
+      end
+    end
   end
 
   def free_upgrade
-    ap 'starting free upgrade'
+    Motion::Blitz.show('Downloading FREE Catalog Update')
     download_and_save
   end
 
@@ -43,6 +73,7 @@ class JewelryDownloader
         File.open(JeweleryData.file_location, 'w') { |file| file.write(json_text) }
         done_downloading
       else
+        Motion::Blitz.error('Download failed. Please try again later.')
         NSLog "Error retrieving data from the #{App.name} server."
       end
     end
@@ -54,6 +85,7 @@ class JewelryDownloader
     ap 'Done Downloading'
     ap JeweleryData.file_data['db']
     App::Persistence['db_version'] = JeweleryData.file_data['db']
+    Motion::Blitz.success('All done!')
   end
 
   def current_version
