b9e292257766639e7d77226aa28efb84645c31d9
Mark Rickert <mjar81@gmail.com>
2014-05-23 14:49:57 -0400

    Half price table working

--- app/Screens/tabs/half_price_screen.rb
+++ app/Screens/tabs/half_price_screen.rb
@@ -6,22 +6,39 @@ class HalfPriceScreen < MasterJewelryScreen
   def build_cell(data)
     {
       selection_style: UITableViewCellSelectionStyleDefault,
-      action: :toggle_half,
-      image: Hostesses.shared_hostess.current_hostess.has_halfprice?(data['item']) ? 'half' : 'normal',
-      arguments: { item: data['item'] },
+      action: :toggle_halfprice,
+      long_press_action: :show_qty_picker,
+      image: Hostesses.shared_hostess.current_hostess.has_halfprice?(data['item']) ? UIImage.cellImageWithText(Hostesses.shared_hostess.current_hostess.halfprice_count(data['item'])) : 'normal',
+      arguments: {
+        item: data['item']
+      },
     }.merge(super)
   end
 
-  def toggle_half(args)
+  def toggle_halfprice(args)
     ch = Hostesses.shared_hostess.current_hostess
 
-    if ch.has_halfprice?(args[:item])
-      ch.remove_halfprice(args[:item])
-    else
-      ch.add_halfprice(args[:item])
-    end
-
-    update_table_data
+    qty = (ch.has_halfprice?(args[:item])) ? 0 : 1
+    ch.set_halfprice(args[:item], qty)
   end
 
+  def show_qty_picker(args)
+    initial_index = 0
+    if Hostesses.shared_hostess.current_hostess.has_halfprice?(args[:item])
+      initial_index = Hostesses.shared_hostess.current_hostess.halfprice_count(args[:item])
+    end
+
+    ActionSheetStringPicker.showPickerWithTitle(
+      "Select Qty",
+      rows: (0..10).to_a.map{ |i| i.to_s },
+      initialSelection: initial_index,
+      doneBlock: -> picker, index, value {
+        ap "Picked Qty: #{value}"
+        Hostesses.shared_hostess.current_hostess.set_halfprice(args[:item], value)
+      },
+      cancelBlock: -> picker {
+        ap "Canceled the picker"
+      },
+      origin: self.view)
+  end
 end
