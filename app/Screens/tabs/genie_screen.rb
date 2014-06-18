ee086cc51cf4deffe3ec087583de531a1ede272c
Mark Rickert <mjar81@gmail.com>
2014-06-17 20:45:38 -0400

    Genie Screen listings done.

--- app/Screens/tabs/genie_screen.rb
+++ app/Screens/tabs/genie_screen.rb
@@ -1,22 +1,50 @@
 class GenieScreen < MasterJewelryScreen
+  longpressable
   title "Jewelry Genie"
   tab_bar_item icon: "tab_genie", title: "Genie"
 
-  # def on_load
-  #   super
-  #   set_nav_bar_button :right, title: "Clear All", action: :clear_all_free, system_item: :trash
-  # end
+  def on_load
+    super
 
-  # def build_cell(data)
-  #   super.merge({
-  #     selection_style: UITableViewCellSelectionStyleDefault,
-  #     action: :toggle_free,
-  #     long_press_action: :show_qty_picker,
-  #     image: Hostesses.shared_hostess.current_hostess.has_free?(data['item']) ? UIImage.cellImageWithText(Hostesses.shared_hostess.current_hostess.free_count(data['item'])) : 'normal',
-  #     arguments: {
-  #       item: data['item']
-  #     }
-  #   })
-  # end
+    placeholder = [{
+      title: 'Loading...',
+      selection_style: UITableViewCellSelectionStyleNone
+    }]
+    @free_data ||= placeholder
+    @halfprice_data ||= placeholder
+
+    set_nav_bar_button :right, title: "Clear All", action: :clear_all, system_item: :trash
+  end
+
+  def table_data
+    [{
+      title: "Half Price Selections",
+      cells: @halfprice_data
+    },{
+      title: "Free Selections",
+      cells: @free_data
+    }]
+  end
+
+  def cells
+    ch = Hostesses.shared_hostess.current_hostess
+
+    free_item_numbers = ch.free_items.collect{ |j| j.item }
+    halfprice_item_numbers = ch.halfprice_items.collect{ |j| j.item }
+
+    @free_data = []
+    JewelryData.items(free_item_numbers).each do |wli|
+      @free_data << build_free_cell(wli)
+    end
+    @free_data << {title: "No Selections"} if @free_data.count == 0
+
+    @halfprice_data = []
+    JewelryData.items(halfprice_item_numbers).each do |wli|
+      @halfprice_data << build_halfprice_cell(wli)
+    end
+    @halfprice_data << {title: "No Selections"} if @halfprice_data.count == 0
+
+    update_table_data
+  end
 
 end
