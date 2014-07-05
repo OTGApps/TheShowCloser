0651212b13346ed31f4b551e629d9b4f1afd4420
Mark Rickert <mjar81@gmail.com>
2014-07-05 12:17:21 -0400

    Add xcproject for editing the core data model

---
+++ DataModelXCProj/TSCDatamodel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TSCDatamodel
+//
+//  Created by Mark Rickert on 1/13/14.
+//  Copyright (c) 2014 Mohawk Apps, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
