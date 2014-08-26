f5a9b2f8635dbfc9a7380e48118097a7198d8ef7
Mark Rickert <mjar81@gmail.com>
2014-08-26 09:07:43 -0500

    Update to use index path from new version of promotion.

--- app/screens/master_jewelry_screen.rb
+++ app/screens/master_jewelry_screen.rb
@@ -94,13 +94,13 @@ class MasterJewelryScreen < PM::TableScreen
 
   #Toggling
 
-  def toggle_free(args)
+  def toggle_free(args, index_path)
     mp args
-    toggle_item(args[:item], true, args[:index_path])
+    toggle_item(args[:item], true, index_path)
   end
 
-  def toggle_halfprice(args)
-    toggle_item(args[:item], false, args[:index_path])
+  def toggle_halfprice(args, index_path)
+    toggle_item(args[:item], false, index_path)
   end
 
   def toggle_item(item, free, index_path)
@@ -164,17 +164,16 @@ class MasterJewelryScreen < PM::TableScreen
 
   # Picker
 
-  def show_free_qty_picker(args)
-    show_qty_picker(args, true)
+  def show_free_qty_picker(args, index_path)
+    show_qty_picker(args, true, index_path)
   end
 
-  def show_halfprice_qty_picker(args)
+  def show_halfprice_qty_picker(args, index_path)
     show_qty_picker(args, false)
   end
 
-  def show_qty_picker(args, free)
+  def show_qty_picker(args, free, index_path)
     item = args[:item]
-    index_path = args[:index_path]
 
     # Get the initial index for the picker
     initial_index = 0
